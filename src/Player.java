46b341fe9fc0439636d6677975858c92aa0d3b38
Mehmet Yaylacı <mehmet.yaylaci@ug.bilkent.edu.tr>
2020-12-04 17:25:55 +0300

    added player attr

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,33 @@ import javafx.scene.paint.Color;
 import java.util.ArrayList;
 
 public class Player {
+    String name;
+    Property[] inventory;
+    int jumpJailRights;
+    double money;
+    int cellIndex;
+    Image avatar;
+    Color avatarColor;
+    boolean isJailed;
+    int ownedBrown;
+    int ownedBlue;
+    int ownedRed;
+    int ownedOrange;
+    int ownedGreen;
+    int ownedYellow;
+    int ownedPink;
+    int ownedCyan;
+    boolean ownsBlue;
+    boolean ownsBrown;
+    boolean ownsRed;
+    boolean ownsOrange;
+    boolean ownsGreen;
+    boolean ownsYellow;
+    boolean ownsPink;
+    boolean ownsCyan;
+    final int BROWN_BLUE_LIMIT = 2;
+    final int REST_LIMIT = 3;
+
     public int getIndex() {
         return 0;
     }
