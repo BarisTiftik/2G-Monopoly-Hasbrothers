f2abdcc728f4eb4bac0258851d3a55c1c5e1ee95
barış tiftik <brstftk@hotmail.com>
2020-12-04 16:45:09 +0300

    Dice düzenleme

--- src/Dice.java
+++ src/Dice.java
@@ -1,9 +1,38 @@
 public class Dice
 {
+    // properties
+    private int diceSum;
+    private boolean isEven;
+
+    // constructors
+    public Dice()
+    {
+        diceSum = 0;
+        isEven = false;
+    }
+
     // methods
     public void rollDice()
     {
-        // rolling
-        // TODO
+        isEven = false;
+
+        int die1 = (int)(Math.random()*6) + 1;
+        int die2 = (int)(Math.random()*6) + 1;
+
+        if (die1==die2)
+            isEven = true;
+
+        diceSum = die1 + die2;
+    }
+
+    // getters
+    public boolean isEven()
+    {
+        return isEven;
+    }
+
+    public int getDiceSum()
+    {
+        return diceSum;
     }
 }
